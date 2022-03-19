1da24118715ca8b696b9427061c0675efaab7de8
Dylan Ascencio <empressglatia@gmail.com>
2022-03-18 22:59:46 -0500

    Copied over decoding functions from j3dultra

---
+++ include/decode.h
@@ -0,0 +1,34 @@
+#ifndef DECODE_H
+#define DECODE_H
+
+#include <stdint.h>
+
+/* Greyscale formats */
+// Decodes the given image with the given dimensions from 4-bit intensity format to RGBA8.
+void btiDecodeI4(uint8_t* src, uint16_t width, uint16_t height, uint8_t* dst);
+// Decodes the given image with the given dimensions from 8-bit intensity format to RGBA8.
+void btiDecodeI8(uint8_t* src, uint16_t width, uint16_t height, uint8_t* dst);
+// Decodes the given image with the given dimensions from 4-bit intensity + alpha format to RGBA8.
+void btiDecodeIA4(uint8_t* src, uint16_t width, uint16_t height, uint8_t* dst);
+// Decodes the given image with the given dimensions from 8-bit intensity + alpha format to RGBA8.
+void btiDecodeIA8(uint8_t* src, uint16_t width, uint16_t height, uint8_t* dst);
+
+/* Color formats */
+// Decodes the given image with the given dimensions from RGB565 format to RGBA8.
+void btiDecodeRGB565(uint8_t* src, uint16_t width, uint16_t height, uint8_t* dst);
+// Decodes the given image with the given dimensions from RGB5A3 format to RGBA8.
+void btiDecodeRGB5A3(uint8_t* src, uint16_t width, uint16_t height, uint8_t* dst);
+// Decodes the given image with the given dimensions from RGBA32 format to RGBA8.
+void btiDecodeRGBA32(uint8_t* src, uint16_t width, uint16_t height, uint8_t* dst);
+
+/* Palette formats */
+// Decodes the given image with the given dimensions from 4-bit paletted format to RGBA8.
+void btiDecodeC4(uint8_t* src, uint16_t width, uint16_t height, uint8_t* palette, uint8_t paletteFormat, uint8_t* dst);
+// Decodes the given image with the given dimensions from 8-bit paletted format to RGBA8.
+void btiDecodeC8(uint8_t* src, uint16_t width, uint16_t height, uint8_t* palette, uint8_t paletteFormat, uint8_t* dst);
+
+/* CMPR (S3TC/DXT1) */
+// Decodes the given image with the given dimensions from DXT1-compressed format to RGBA8.
+void btiDecodeCMPR(uint8_t* src, uint16_t width, uint16_t height, uint8_t* dst);
+
+#endif
